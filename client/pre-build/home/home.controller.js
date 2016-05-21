b9de2740fb2598d51bb4f1446ca9000035c62c3e
Ash <ash@ashryanbeats.com>
2016-05-21 19:24:40 -0400

    Listing 10 things to make great again.

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -1,15 +1,18 @@
 app.controller('HomeController', function($scope, $http, HomeFactory) {
   
-  $scope.msgFromScope = "...And I'm a message from the HomeController scope, just so you know that I work!";
+	$scope.ungreatThings;
 
-  $scope.getUngreatThings = function() {
-  	HomeFactory.getUngreatThings()
-  		.then(function(data) {
-  			console.log(data);
-  		})
-  		.catch(function(err) {
-  			console.error(err);
-  		});
-  }
+	$scope.getUngreatThings = function() {
+		HomeFactory.getUngreatThings()
+			.then(function(data) {
+
+				$scope.ungreatThings = data;
+				console.log($scope.ungreatThings);
+
+			})
+			.catch(function(err) {
+				console.error(err);
+			});
+	}
 
 });
